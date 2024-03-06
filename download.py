936e88e5f947a172838962f1cd0a3b74e0b3f1f4
Suraj Joshi <joshisur231@gmail.com>
2024-03-06 11:20:38 +0545

    add support for mosiac image collection with single date eg:esri lulc, error handeling and logging

--- download.py
+++ download.py
@@ -3,16 +3,21 @@ import geemap
 import os
 from datetime import datetime
 from pathlib import Path
+import logging
 
 #Change the following two lines from your GEE account:
 ee.Authenticate() #Will ask for your google earth engine credentials. Comment this line after running this once.
 ee.Initialize(project="ee-joshisur231") #instead of "ee.joshisur231" use your project name (top right corner of the GEE code editor)
+
+logging.basicConfig(filename='log/error.log', level=logging.ERROR)
 
 def download_image(
         image_asset_address, 
         clip=True, 
         roi=ee.FeatureCollection("projects/ee-joshisur231/assets/pa_effectiveness/nepal_boundary").geometry(), 
         output_path=None,
+        mosaic_collection = False,
+        spatial_resolution = None,
         **kwargs
         ):
     """
@@ -22,6 +27,7 @@ def download_image(
     - image_asset_address (str): A link to the image in Earth Engine.
     - roi (ee.FeatureCollection): A region of interest (ROI) specified as an Earth Engine FeatureCollection. Defaults to Nepal Boundary.
     - output_path (str): The file path to save the downloaded GeoTIFF image.
+    - mosaic_collection (bool, optional): In case the there are multiple tiles in the image. Default is False.
     - clip (bool, optional): Whether to clip the image to the specified ROI. Default is True.
     - Optional parameters that can be passed if required (refer to geemap documentation for details): 
         -resampling = "near", (use 'bilinear', 'bicubic' for soomothing continous data)
@@ -39,30 +45,43 @@ def download_image(
     Note:
     - Required packages: geemap, geedim and earth-engine
     """
+    try:
+        image_asset_address = str(image_asset_address)
+        image_prefix = image_asset_address.split("/")[0]
+        
+        if mosaic_collection:
+            image_collection = ee.ImageCollection(image_asset_address)
+            spatial_resolution = image_collection.first().projection().nominalScale()
+            crs = "EPSG:4326"
+            image = image_collection.mosaic()
 
-    image_asset_address = str(image_asset_address)
-    image = ee.Image(image_asset_address)
-    image_prefix = image_asset_address.split("/")[0]
-    spatial_resolution = image.projection().nominalScale().getInfo()
+        else:
+            image = ee.Image(image_asset_address)
+            spatial_resolution = image.projection().nominalScale().getInfo()
+            crs = None
+        
+        if clip:
+            image = image.clip(roi)
 
-    if clip:
-        image = image.clip(roi)
+        if output_path is None:
+            downloads_dir = str(Path.home() / "Downloads")
+            output_path = os.path.join(downloads_dir, f"{image_prefix}_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.tif")
+        else:
+            output_dir = os.path.dirname(output_path)
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
 
-    if output_path is None:
-        downloads_dir = str(Path.home() / "Downloads")
-        output_path = os.path.join(downloads_dir, f"{image_prefix}_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.tif")
-    else:
-        output_dir = os.path.dirname(output_path)
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
-
-    return geemap.download_ee_image(
-        image = image,
-        filename = output_path,
-        region = roi,
-        scale = spatial_resolution,
-        **kwargs
-        )
+        return geemap.download_ee_image(
+            image = image,
+            filename = output_path,
+            region = roi,
+            crs=crs,
+            scale = spatial_resolution,
+            **kwargs
+            )
+    except Exception as e:
+        logging.error(f"Error: ({datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}): {e} for asset {image_asset_address}")
+        raise e
 
 def download_image_collection(
         collection_asset_address, 
@@ -71,6 +90,7 @@ def download_image_collection(
         output_path=None, 
         roi=ee.FeatureCollection("projects/ee-joshisur231/assets/pa_effectiveness/nepal_boundary").geometry(), 
         clip=True,
+        mosaic = False,
         **kwargs
         ):
     """
@@ -101,36 +121,40 @@ def download_image_collection(
     """
     def clip(image):
         return image.clip(roi)
-    
-    image_prefix = collection_asset_address.split("/")[0]
-    
-    collection_asset_address = str(collection_asset_address)
-    image_collection = ee.ImageCollection(collection_asset_address)\
-        .filterBounds(roi)\
-        .filterDate(start_date, end_date)
-    
-    spatial_resolution = image_collection.first().projection().nominalScale().getInfo()
+    try:
+        image_prefix = collection_asset_address.split("/")[0]
+        
+        collection_asset_address = str(collection_asset_address)
+        
+        image_collection = ee.ImageCollection(collection_asset_address)\
+            .filterBounds(roi)\
+            .filterDate(start_date, end_date)
+        
+        spatial_resolution = image_collection.first().projection().nominalScale().getInfo()
 
-    if clip:
-        image_collection = image_collection.map(clip)
+        if clip:
+            image_collection = image_collection.map(clip)
 
-    if output_path is None:
-        downloads_dir = str(Path.home() / "Downloads")
-        output_path = os.path.join(downloads_dir, f"{image_prefix}_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}")
-    else:
-        output_dir = os.path.dirname(output_path)
-        if not os.path.exists(output_dir):
-            os.makedirs(output_dir)
+        if output_path is None:
+            downloads_dir = str(Path.home() / "Downloads")
+            output_path = os.path.join(downloads_dir, f"{image_prefix}_{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}")
+        else:
+            output_dir = os.path.dirname(output_path)
+            if not os.path.exists(output_dir):
+                os.makedirs(output_dir)
 
-    return geemap.download_ee_image_collection(
-        collection = image_collection,
-        out_dir = output_path,
-        region = roi,
-        scale = spatial_resolution,
-        **kwargs
-        )
+        return geemap.download_ee_image_collection(
+            collection = image_collection,
+            out_dir = output_path,
+            region = roi,
+            scale = spatial_resolution,
+            **kwargs
+            )
+    except Exception as e:
+        logging.error(f"Error: ({datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}): {e} for asset {collection_asset_address}")
+        raise e
 
-def add_image_to_map(image_path = None, image_assest_address = None, layer_name = None, **kwargs):
+def add_image_to_map(image_path = None, image_asset_address = None, layer_name = None, **kwargs):
     """
     Adds Earth Engine assests vector/images and local rasters to an interactive map.
 
@@ -146,8 +170,7 @@ def add_image_to_map(image_path = None, image_assest_address = None, layer_name 
     if image_path:
         Map.add_raster(source = image_path, layer_name = layer_name, **kwargs)
     
-    if image_assest_address:
-        Map.addLayer(ee_object = ee.Image(image_assest_address), name = layer_name, **kwargs)
-    
+    if image_asset_address:
+        Map.addLayer(ee_object = ee.Image(image_asset_address), name = layer_name, **kwargs)
     return Map
     
